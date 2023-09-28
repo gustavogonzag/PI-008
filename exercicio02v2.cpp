0db1809e7a1ca37a9b9c24bcdb61cde8781e5131
Luiz Felipe <luizfelipecrx@gmail.com>
2023-09-28 18:41:20 -0300

    Implementa simulação de notas

--- exercicio02v2.cpp
+++ exercicio02v2.cpp
@@ -6,7 +6,8 @@ const int NUM_ESTACOES = 250;
 const int TEMPERATURA_MINIMA = 10;
 const int TEMPERATURA_MAXIMA = 40;
 
-int main() {
+int main()
+{
     // Inicializa o gerador de números aleatórios
     std::srand(std::time(nullptr));
 
@@ -14,34 +15,43 @@ int main() {
     int temperaturas[NUM_ESTACOES];
 
     // Gera as temperaturas aleatoriamente entre 10 e 40
-    for (int i = 0; i < NUM_ESTACOES; i++) {
+    for (int i = 0; i < NUM_ESTACOES; i++)
+    {
         temperaturas[i] = std::rand() % (TEMPERATURA_MAXIMA - TEMPERATURA_MINIMA + 1) + TEMPERATURA_MINIMA;
     }
 
     // Determina a temperatura máxima e mínima
     int temperaturaMaxima = temperaturas[0];
     int temperaturaMinima = temperaturas[0];
-    for (int i = 1; i < NUM_ESTACOES; i++) {
-        if (temperaturas[i] > temperaturaMaxima) {
+    for (int i = 1; i < NUM_ESTACOES; i++)
+    {
+        if (temperaturas[i] > temperaturaMaxima)
+        {
             temperaturaMaxima = temperaturas[i];
         }
-        if (temperaturas[i] < temperaturaMinima) {
+        if (temperaturas[i] < temperaturaMinima)
+        {
             temperaturaMinima = temperaturas[i];
         }
     }
 
     // Determina a temperatura média
     int somaTemperaturas = 0;
-    for (int i = 0; i < NUM_ESTACOES; i++) {
+    for (int i = 0; i < NUM_ESTACOES; i++)
+    {
         somaTemperaturas += temperaturas[i];
     }
     double temperaturaMedia = static_cast<double>(somaTemperaturas) / NUM_ESTACOES;
 
     // Atualiza as temperaturas de acordo com a previsão do modelo
-    for (int i = 0; i < NUM_ESTACOES; i++) {
-        if (temperaturas[i] > temperaturaMedia) {
+    for (int i = 0; i < NUM_ESTACOES; i++)
+    {
+        if (temperaturas[i] > temperaturaMedia)
+        {
             temperaturas[i] += 1;
-        } else if (temperaturas[i] < temperaturaMedia) {
+        }
+        else if (temperaturas[i] < temperaturaMedia)
+        {
             temperaturas[i] -= 2;
         }
     }
